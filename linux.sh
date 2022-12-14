d249ed882d5bbdb2665a79f66ed1c0c5dde6744a
Maxime Daniel <root@maxux.net>
2022-12-14 16:17:31 +0100

    linux: add build note

---
+++ linux.sh
@@ -0,0 +1,8 @@
+#!/usr/bash
+
+# build library
+gcc jerasure-simple.c -shared -Wl,-soname,libjerasuresimple.so -o libjerasuresimple.so -I/usr/include/jerasure -fpic
+
+# build jeratest.c with the library
+gcc jeratest.c -o jeratest -I/usr/include/jerasure -lJerasure -lm -L/mnt/jerasure-simple -ljerasuresimple -lm -lJerasure
+
