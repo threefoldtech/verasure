dd5556307f3bd0fada6859a5de9f607eb3ac59b8
Maxime Daniel <root@maxux.net>
2022-12-14 16:18:45 +0100

    linux: add v build command

--- linux.sh
+++ linux.sh
@@ -6,3 +6,5 @@ gcc jerasure-simple.c -shared -Wl,-soname,libjerasuresimple.so -o libjerasuresim
 # build jeratest.c with the library
 gcc jeratest.c -o jeratest -I/usr/include/jerasure -lJerasure -lm -L/mnt/jerasure-simple -ljerasuresimple -lm -lJerasure
 
+# build v version
+LDFLAGS="-L$(pwd) -ljerasuresimple -lJerasure -lm" v -cg jeratest.v
